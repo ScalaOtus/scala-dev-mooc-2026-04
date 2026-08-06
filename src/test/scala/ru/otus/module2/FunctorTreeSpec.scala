83898e20dcc7652a2b4dd8a13379883239c78950
dkochnev <dekochnev@yandex.ru>
2026-08-06 14:11:00 +0300

    Functor2 done again +2

--- src/test/scala/ru/otus/module2/FunctorTreeSpec.scala
+++ src/test/scala/ru/otus/module2/FunctorTreeSpec.scala
@@ -80,7 +80,7 @@ class FunctorTreeSpec extends AnyFlatSpec with Matchers with OptionValues {
     val f = (x: Int) => x * 2
     val g = (y: Int) => y + 1
 
-    val leftSide = simpleTree.map(f andThen g)
+    val leftSide = simpleTree.map(g compose f)
     val rightSide = simpleTree.map(f).map(g)
 
     leftSide shouldBe rightSide
